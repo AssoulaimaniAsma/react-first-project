48944abe79c133eeefaa3fef00de247ae1c63897
AssoulaimaniAsma <asma.assoulaimani@edu.uiz.ac.ma>
2025-03-17 12:26:12 +0000

    Ajout de la page sign up avec design et fonctionnalités

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { useRef } from "react";
+import Signin from "./composante/signin";
+import Signup from "./composante/signup";
 import Navbar from "./composante/Navbar";
-import Signin from './composante/signin';
+import "./App.css";
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  // 🔥 Crée des refs pour éviter findDOMNode()
+  const signinRef = useRef(null);
+  const signupRef = useRef(null);
+
+  return (
+    <TransitionGroup>
+      <CSSTransition 
+        key={location.pathname} 
+        classNames="fade" 
+        timeout={500} 
+        nodeRef={location.pathname === "/signup" ? signupRef : signinRef}
+      >
+        <div ref={location.pathname === "/signup" ? signupRef : signinRef}>
+          <Routes location={location}>
+            <Route path="/" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+}
+
 function App() {
   return (
     <Router>
-    <Navbar />
-    <div>
-          <signin />
-  
-      
-    </div>
-  </Router>
+      <Navbar />
+      <AnimatedRoutes />
+    </Router>
   );
 }
 
