f99e023e8c6062daa28400b28b3c42fd9922eefa
khadijaAitHafid <khadijaaithafid1@gmail.com>
2025-03-21 22:10:11 +0000

    Add files via upload

---
+++ src/composante/CartPage/CartPage.jsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import "./CartPage.css";
+
+function CartPage(){
+    const Products =[
+        {id: 1 , image: "URL1", name: "item1" , price: 180, quantity : 1 },
+        {id: 2 , image: "URL1", name: "item2" , price: 40, quantity : 1 },
+        {id: 3 , image: "URL1", name: "item3" , price: 80, quantity : 1 }
+    ];
+    const recommendation=[
+        {id:"x", image: require("./pizza.png"), name: "item x", oldPrice:200, newPrice: 140},
+        {id:"y", image: require("./pizza.png"), name: "item y", oldPrice:230, newPrice: 200},
+        {id:"z", image: require("./pizza.png"), name: "item z", oldPrice:200, newPrice: 140},
+        {id:"z", image: require("./pizza.png"), name: "item z", oldPrice:200, newPrice: 140},
+        {id:"z", image: require("./pizza.png"), name: "item z", oldPrice:200, newPrice: 140}
+    ];
+    //initialiser l etat cart avec la valeur de products
+    //cart stocke la liste actuelle des produits ajoutes au panier
+    //setCart est fct qui permet de mettre a jour l etat cart
+    const [cart, setCart]=useState(Products);
+    const UpdateQuantity = (id,amount) => {
+        setCart(cart.map(item =>
+            //...item copie toutes les proprietes de item
+            //Math.max(1, ...) ➝ Empêche la quantité de descendre sous 1.
+            item.id === id ? { ...item, quantity: Math.max(1, item.quantity + amount) } : item
+        ));
+    };
+    const RemoveItem =(id) =>{
+        //cart.filter(...) Crée un nouveau tableau contenant seulement les produits dont l'id est différent de id.
+        setCart(cart.filter(item => item.id !== id));
+    };
+    
+    //acc garde la somme totale au fil du parcours.
+    const subtotal = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+    const discount=subtotal*0.2;
+    const total = subtotal - discount;
+
+    return(
+        <div className="cartContainer">
+            <table className="Product">
+                <thead>
+                    <tr className="headTable">
+                        <th></th>
+                        <th>Product</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {cart.map(item=>(
+                    <tr key={item.id}>
+                        <td>
+                            <button onClick={()=>RemoveItem(item.id)}>X</button>
+                        </td>
+                        <td>
+                            <img src={item.image} alt={item.name} width="50" height="50"/>
+                            {item.name}
+                        </td>
+                        <td>{item.price}DH</td>
+                        <td className="quantityContent">
+                            <button onClick={()=>UpdateQuantity(item.id,-1)}>-</button>
+                            {item.quantity}
+                            <button onClick={()=>UpdateQuantity(item.id,1)}>+</button>
+                        </td>
+                        <td>
+                            {item.price*item.quantity}DH
+                        </td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
+            <table className="TotalPrice">
+                <thead className="headTable">
+                    <th>Cart Total</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>
+                            <td>SUBTOTAL</td>
+                            <td className="tdContent">{subtotal.toFixed(2)} DH</td>
+                        </th>
+                    </tr>
+                    <tr>
+                        <th>
+                            <td>DISCOUNT</td>
+                            <td className="tdContent">{discount.toFixed(2)} DH</td>
+                        </th>
+                    </tr>
+                    <tr>
+                        <th>
+                            <td>TOTAL</td>
+                            <td className="tdContent1">{total.toFixed(2)} DH</td>
+                        </th>
+                    </tr>
+                </tbody>
+            </table>
+            <h2>You May Also Like</h2>
+            <div className="imageContent">
+                {recommendation.map(item=>(
+                    <div className="imageItem" key={item.id}>
+                        <span className="discountBadge">13%</span>
+                        <img src={item.image} alt={item.name} width="200" height="200" />
+                        <div>{item.name}</div>
+                        <div className="PriceContainer">
+                            <div className="oldPrice">{item.oldPrice}</div>
+                            <div className="newPrice">DH{item.newPrice}</div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+export default CartPage;
