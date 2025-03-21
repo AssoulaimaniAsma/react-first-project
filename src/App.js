ecb9068061a43465c75b6644445158755ea810dc
AssoulaimaniAsma <asma.assoulaimani@edu.uiz.ac.ma>
2025-03-21 21:17:59 +0000

    Résolution de conflit et mise à jour

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { useRef } from "react";
-<<<<<<< HEAD
-import Signin from "./composante/signin";
-import Signup from "./composante/signup";
-import Navbar from "./composante/Navbar";
-import CartPage from "./composante/CartePage";
-import ContactForm from "./composante/feed_back";
-import Home from "./composante/Home";
-=======
 import Signin from "./composante/Signin/signin";
 import Signup from "./composante/Signup/signup";
 import Navbar from "./composante/Navbar/Navbar";
 import ContactForm from "./composante/feed_back/feed_back";
 import Home from "./composante/home/Home";
 import AccountSettings from "./composante/Account_Setting/Account_Setting";
->>>>>>> 5723639 (Account settings)
+import CartPage from "./composante/CartePage";  // Garder cette ligne si elle est nécessaire
 import "./App.css";
 
 function AnimatedRoutes() {
   const location = useLocation();
-
-  // 🔥 Crée des refs pour éviter findDOMNode()
   const signinRef = useRef(null);
   const signupRef = useRef(null);
   const contactRef = useRef(null);
@@ -42,7 +32,7 @@ function AnimatedRoutes() {
           signinRef
         }
       >
-        <div  ref={
+        <div ref={
           location.pathname === "/signup" ? signupRef :  
           location.pathname === "/contact" ? contactRef : 
           location.pathname === "/home" ? homeRef :
@@ -53,7 +43,7 @@ function AnimatedRoutes() {
             <Route path="/" element={<Signin />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/contact" element={<ContactForm />} />
-            <Route path="/CartPage" element={<CartPage/>} />
+            <Route path="/CartPage" element={<CartPage />} />
             <Route path="/home" element={<Home />} />
             <Route path="/account" element={<AccountSettings />} />
           </Routes>
@@ -62,23 +52,21 @@ function AnimatedRoutes() {
     </TransitionGroup>
   );
 }
-function Main() {
-  const location = useLocation(); // Utilisez useLocation ici
 
+function Main() {
+  const location = useLocation(); 
   return (
     <>
-      {/* Affichez Navbar uniquement si le chemin n'est ni "/" ni "/signup" */}
       {location.pathname !== "/" && location.pathname !== "/signup" && <Navbar />}
       <AnimatedRoutes />
     </>
   );
 }
+
 function App() {
-  
   return (
     <Router>
-       <Main />
-      
+      <Main />
     </Router>
   );
 }
